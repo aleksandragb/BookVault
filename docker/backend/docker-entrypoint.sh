4cb6187cb0aa957de4896b9266ec7bd1e1b16257
Mateusz Flis <flismati.s@gmail.com>
2025-04-29 01:59:17 +0200

    init: project structure and Docker configuration

---
+++ docker/backend/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! -d vendor/ ]; then
+    echo "Installing dependencies..."
+    composer install --prefer-dist --no-progress --no-interaction
+fi
+
+php bin/console -V
+
+if grep -q ^DATABASE_URL= .env; then
+   echo 'Waiting for database to be ready...'
+   ATTEMPTS_LEFT_TO_REACH_DATABASE=10
+   until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
+       sleep 1
+       ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+       echo "Still waiting for database to be ready... $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+   done
+
+   if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+       echo 'The database is not up or not reachable:'
+       echo "$DATABASE_ERROR"
+       exit 1
+   else
+       echo 'The database is now ready and reachable'
+   fi
+
+   if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
+       php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+   fi
+fi
+
+exec docker-php-entrypoint "$@"
